6835eb3f98c81dbc3c00553a2ae27b3cbb35d0e3
the null <xinyechenai@gmail.com>
2024-04-21 20:34:08 +0200

    Add files via upload

---
+++ @hodlr/hlu.m
@@ -0,0 +1,23 @@
+function [L, U] = hlu(A)
+    if ~issqaure(A)
+        error('LU factorization is only applied to a square HODLR matrix.');
+    end
+    
+    [m, n, m1, m2, n1, n2] = hsize(A);
+    
+    if isempty(A.D)
+        [L11, U11] = hlu(A.A11);
+        U12 = mldivide(L11, A.U1 * A.V2); % L11 U12 = A12
+        L21 = mrdivide(A.U2 * A.V1, U11); % L21 U11 = A21
+        [L22, U22] = lu(hadd(A.A22, L21 * U12, '-'));
+        
+        L = blkdiag(L11, L22);
+        L(size(L11, 1)+1:end, 1:size(L11, 2)) = L21;
+        L = sparse(L);
+        U = blkdiag(U11, U22);
+        U(1:size(U11, 1), size(U11, 2)+1:end) = U12;
+        U = sparse(U);
+    else
+        [L, U] = lu(A.D);
+    end
+end 
--- @hodlr/hsize.m
+++ @hodlr/hsize.m
@@ -1,9 +1,14 @@
-function [m, n] = hsize(A)
+function [m, n, varargout] = hsize(A)
     su1 = size(A.U1, 1);
     su2 = size(A.U2, 1);
     sv1 = size(A.V1, 2);
     sv2 = size(A.V2, 2);
-    
     m = su1 + su2;
     n = sv1 + sv2;
+
+    nout = max(nargout, 1) - 2;
+    if nargout > 1
+        varargout = {su1, su2, sv1, sv2};
+    end
+
 end

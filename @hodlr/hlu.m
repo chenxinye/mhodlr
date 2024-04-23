b0dcc528a4553641a2979c170ae2661261de3ac3
the null <xinyechenai@gmail.com>
2024-04-23 17:04:48 +0200

    update lu

--- @hodlr/hlu.m
+++ @hodlr/hlu.m
@@ -1,4 +1,4 @@
-function [L, U] = hlu(H, threshold)
+function [L, U] = hlu(H, epsilon)
     if ~issquare(H)
         error('LU factorization is only applied to a square HODLR matrix.');
     end
@@ -6,15 +6,15 @@ function [L, U] = hlu(H, threshold)
     [m, n, m1, m2, n1, n2] = hsize(H);
     
     if isempty(H.D)
-        [L11, U11] = hlu(H.A11, threshold);
+        [L11, U11] = hlu(H.A11, epsilon);
         % U12 = mldivide(L11, H.U1 * H.V2); % L11 U12 = A12 = H.U1 * H.V2
         % L21 = mrdivide(H.U2 * H.V1, U11); % L21 U11 = A21 = H.U2 * H.V1
         U12 = mldivide(L11, H.U1); 
         L21 = mrdivide(H.V1, U11); 
 
         X = -H.U2 * (L21 * U12); 
-        NH = hrank_update(H.A22, X, H.V2, threshold);
-        [L22, U22] = hlu(NH, threshold); % lu(hadd(H.A22, L21 * U12, '-'));
+        NH = hrank_update(H.A22, X, H.V2, epsilon);
+        [L22, U22] = hlu(NH, epsilon); % lu(hadd(H.A22, L21 * U12, '-'));
         
         U12 = U12 * H.V2;
         L21 = H.U2 * L21;

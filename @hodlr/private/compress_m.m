15e95b2e193939dac5c344adf95b45185440dda3
the null <xinyechenai@gmail.com>
2024-04-24 15:20:08 +0200

    Add files via upload

--- @hodlr/private/compress_m.m
+++ @hodlr/private/compress_m.m
@@ -1,5 +1,5 @@
 %% FOR TEST ONLY
-function [U, V] = compress_m(A, method, threshold)
+function [U, V] = compress_m(A, method, epsilon)
     if min(size(A)) == 0
         U = zeros(size(A,1),0);
         V = zeros(0,size(A,2));
@@ -8,13 +8,13 @@ function [U, V] = compress_m(A, method, threshold)
 
     if strcmp(method, 'svd')
         [U, S, V] = svd(full(A), 'econ');
-        k = sum(abs(diag(S)) > S(1,1) * threshold);
-        U = U(:,1:k) * S(1:k,1:k);
-        V = V(:,1:k)';
+        rnk = sum(abs(diag(S)) > S(1,1) * epsilon);
+        U = U(:,1:rnk) * S(1:rnk,1:rnk);
+        V = V(:,1:rnk)';
     elseif strcmp(method, 'qr')
         [U, V, P] = qr(A);
-        k = sum(abs(diag(V)) > V(1,1) * threshold);
-        U = U(:, 1:k);
-        V = V(1:k,:)*P;
+        rnk = sum(abs(diag(V)) > V(1,1) * epsilon);
+        U = U(:, 1:rnk);
+        V = V(1:rnk,:)*P;
     end
 end

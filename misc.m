33b72ff7b0a5aed5430c4117aee98b4ed7bff282
the null <xinyechenai@gmail.com>
2024-04-28 17:13:06 +0200

    Update misc.m

--- misc.m
+++ misc.m
@@ -1,12 +1,39 @@
 n = 10;
+addpath('tests');
+addpath('@hodlr');
+setenv("hodlr","tests");
+test_recover
+
+test_chol
+test_lu
+
+unittests
+
+
 A = spdiags(ones(n, 1) * [1 3  -1],  -1:1, n, n);
 B = spdiags(ones(n, 1) * [1 3  1],  -1:1, n, n);
+C = spdiags(ones(n, 1) * [1 3  -1],  -1:1, 3, 3);
+D = spdiags(ones(n, 1) * [1 3  -1],  -1:1, 4, 4);
+
+Cs = rand(4,4,3);
+testA = Cs(:, :, 1);
+R = chol(testA' * testA)
+Q = mrdivide(testA, R)
+RA = Q*R
+
+H = blkoffdiag(Cs)
+H = blkoffdiag(C, D)
+
+
 
 [U, V] = compress_test(A(1:5, 5:end), ...
     'svd', 1e-8);
 norm(U * V - A(1:5, 5:end), 2);
-
-hA = hodlr(A);
+;
+hA = hodlr(Cs(:,:, 1));
+invhA = hA.inverse_nonrecursive_hodlr();
+invhA = hA.inverse_nonrecursive_double();
+norm(full(invhA * Cs(:,:, 1) - eye(4)),2)
 
 hB = hodlr(B);
 R = hchol(hB);
@@ -21,8 +48,6 @@ hC4 = hadd(A, B, '+')
 
 thred = 10^(-10);
 % [Un,Vn] = compress_factors(U, V, thred)
-
-
 [L, U] = hlu(hA, 10^(-10));
 
 norm(full(L * U - A),2)

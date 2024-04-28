036300d49e4fd07210f01a59f07ebb83619ac4c9
the null <xinyechenai@gmail.com>
2024-04-28 17:12:28 +0200

    Update unittests.m

--- unittests.m
+++ unittests.m
@@ -1,1 +1,15 @@
+function b = unittests()
+    if ~test_recover()
+        error('Do not pass test 1.')
+    end
 
+    if ~test_chol()
+        error('Do not pass test 1.')
+    end
+    
+    if ~test_lu()
+        error('Do not pass test 1.')
+    end
+    
+    return 1;
+end

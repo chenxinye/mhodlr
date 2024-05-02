95651dd957d058d62a86b341d175cab250ac3ba2
the null <xinyechenai@gmail.com>
2024-05-02 11:30:12 +0200

    add hodlr code

---
+++ mphodlr/hchol.m
@@ -0,0 +1,60 @@
+function R = hchol(H, varargin)
+%% Compute Cholesky factorization for symmetric positive-definite HODLR matrix H.
+%
+% Parameters
+% --------------------
+% H - hodlr
+%     Matrix in HODLR format - hodlr class.
+% 
+% epsilon - double
+%     The threshold for recompression.
+%
+%
+% Returns
+% --------------------
+% R - double
+%     The upper triangular matrix R is computed such that R' * R = H. 
+% 
+    if nargin < 2
+        epsilon = 1.0e-12;
+    else 
+        epsilon = varargin{1};
+    end
+    
+    if isempty(H.D)
+        [m, n, m1, m2, n1, n2] = hsize(H);
+        R11 = hchol(H.A11, epsilon);
+        R12 = mldivide(R11', H.U1 * H.V2);
+        R22 = hchol(hrank_update(H.A22, -R12', R12, epsilon), epsilon);
+        
+        R = [R11, R12; zeros(m2, n1), R22];
+    else
+        R = chol(H.D);
+    end
+end
+
+%{  
+
+% Compare below
+
+function R = hchol(H, varargin)
+
+
+    if nargin < 2
+        epsilon = 1.0e-12;
+
+    if isempty(H.D)
+        [m, n, m1, m2, n1, n2] = hsize(H);
+        R11 = hchol(H.A11);
+        R12 = mldivide(R11', H.U1*H.V2);
+        R22 = hchol(hadd_partial_hodlr(H.A22, R12' * R12, '-'));
+        R = [R11, R12; zeros(m2, n1), R22];
+   else
+        R = chol(H.D);
+   end
+end
+
+
+
+ %}
+
---
+++ mphodlr/issquare.m
@@ -0,0 +1,31 @@
+function ch = issquare(H)
+%% The function is to check whether HODLR matrix H is square or not.
+%
+% Parameters
+% --------------------
+% H - hodlr
+%     Matrix in HODLR format - hodlr class.
+% 
+%
+% Returns
+% --------------------
+% ch - boolean
+%     `1` indicates H is square matrix, `0` indicates otherwise. 
+% 
+    if class(H) == 'hodlr'
+        su1 = size(H.U1, 1);
+        su2 = size(H.U2, 1);
+        sv1 = size(H.V1, 2);
+        sv2 = size(H.V2, 2);
+        
+        m = su1 + su2;
+        n = sv1 + sv2;
+        ch = m == n;
+        
+    elseif class(H) == 'double' or 'single'
+        [m, n] = size(H);
+        ch = m == n;
+    else
+        error('Please enter correct type of input.');
+    end
+end

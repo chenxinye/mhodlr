386a9fe916e31a2b2efe93fad1c9e48180d08dd2
the null <xinyechenai@gmail.com>
2024-05-03 09:28:10 +0200

    update hdot

--- @mphodlr/private/mphdot_double.m
+++ @mphodlr/private/mphdot_double.m
@@ -1,4 +1,4 @@
-function C = hdot_double(A, B)
+function C = mphdot_double(A, B)
     if isa(A, 'hodlr') & isa(B, 'hodlr')
         [mA, nA] = hsize(A);
         [mB, nB] = hsize(B);
@@ -6,17 +6,19 @@ function C = hdot_double(A, B)
             error('Please enter the inputs with correct dimensions.');
         end
 
+        set_prec(A.prec_settings{A.level});
+        
         if isempty(A.D) & isempty(B.D)
-            A12 = A.U1 * A.V2;
-            A21 = A.U2 * A.V1;
+            A12 = mchop(mchop(A.U1) * mchop(A.V2));
+            A21 = mchop(mchop(A.U2) * mchop(A.V1));
             
-            B12 = B.U1 * B.V2;
-            B21 = B.U2 * B.V1;
+            B12 = mchop(mchop(B.U1) * mchop(B.V2));
+            B21 = mchop(mchop(B.U2) * mchop(B.V1));
                 
-            C11 = hdot_double(A.A11, B.A11, 'double') + A12 * B21;
+            C11 = hdot_double(A.A11, B.A11, 'double') + mchop(A12 * B21);
             C12 = hdot_double(A.A11, B12, 'double') + hdot_double(A12, B.A22, 'double');
             C21 = hdot_double(A21, B.A11, 'double') + hdot_double(A.A22, B21, 'double');
-            C22 = A21 * B12 + hdot_double(A.A22, B.A22, 'double');
+            C22 = mchop(A21 * B12) + hdot_double(A.A22, B.A22, 'double');
 
             C = [C11, C12; C21, C22];
         
@@ -27,6 +29,8 @@ function C = hdot_double(A, B)
         end
             
     elseif isa(A, 'hodlr')
+        set_prec(A.prec_settings{A.level});
+
         if isempty(A.D)
             [mA, nA, su1, su2, sv1, sv2] = hsize(A);
             [mB, nB] = size(B);
@@ -37,17 +41,19 @@ function C = hdot_double(A, B)
         
             C = zeros(mA, nB);
             y1 = hdot_double(A.A11, B(1:sv1, :), 'double');
-            y2 = A.U1 * A.V2 * B(sv1+1:end, :);
-            y3 = A.U2 * A.V1 * B(1:sv1, :);
+            y2 = mchop(mchop(mchop(A.U1) * mchop(A.V2)) * mchop(B(sv1+1:end, :)));
+            y3 = mchop(mchop(mchop(A.U2) * mchop(A.V1)) * mchop(B(1:sv1, :)));
             y4 = hdot_double(A.A22, B(sv1+1:end, :), 'double');
             
-            C(1:su1, :) = y1 + y2;
-            C(su1+1:end, :) = y3 + y4;
+            C(1:su1, :) = mchop(y1 + y2);
+            C(su1+1:end, :) = mchop(y3 + y4);
         else
-            C = A.D * B;
+            C = mchop(mchop(A.D) * mchop(B));
         end
 
     elseif isa(B, 'hodlr')
+        set_prec(B.prec_settings{B.level});
+
         if isempty(B.D)
             [mB, nB, su1, su2, sv1, sv2] = hsize(B);
             [mA, nA] = size(A);
@@ -58,17 +64,17 @@ function C = hdot_double(A, B)
         
             C = zeros(mA, nB);
             y1 = hdot_double(A(:, 1:su1), B.A11, 'double');
-            y2 = A(:, su1+1:end) * B.U2 * B.V1;
-            y3 = A(:, 1:su1) * B.U1 * B.V2;
+            y2 = mchop(mchop(mchop(A(:, su1+1:end)) * mchop(B.U2)) * mchop(B.V1));
+            y3 = mchop(mchop(mchop(A(:, 1:su1)) * B.U1) * B.V2);
             y4 = hdot_double(A(:, su1+1:end), B.A22, 'double');
             
-            C(:, 1:sv1) = y1 + y2;
-            C(:, sv1+1:end) = y3 + y4;
+            C(:, 1:sv1) = mchop(y1 + y2);
+            C(:, sv1+1:end) = mchop(y3 + y4);
         else
-            C = A * B.D;
+            C = mchop(mchop(A) * mchop(B.D));
         end
         
     else
-        C = A * B;
+        C = mchop(mchop(A) * mchop(B));
     end
 end

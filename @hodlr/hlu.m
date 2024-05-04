e1ac7ac9c81e5624dfb2e7ec86d969e89a48323e
the null <xinyechenai@gmail.com>
2024-05-04 19:00:35 +0200

    update LU factorization

--- @hodlr/hlu.m
+++ @hodlr/hlu.m
@@ -7,9 +7,11 @@ function [L, U] = hlu(H, varargin)
     H - hodlr
         Matrix in HODLR format - hodlr class.
     
-    epsilon - double
+    epsilon - double, default=10^(-10)
         The threshold for recompression.
 
+    oformat - str, default='hodlr'
+        The output format. 'dense' or 'hodlr'.
 
     Returns
     --------------------
@@ -24,34 +26,66 @@ function [L, U] = hlu(H, varargin)
     end
     
     if nargin == 1
-        epsilon = 10^(-10);
-    else 
-        epsilon = varargin{1};
+        oformat = 'hodlr';
+        epsilon = 1.0e-12;
+    elseif nargin == 2 
+        oformat = varargin{1};
+        epsilon = 1.0e-12;
+    else
+        oformat = varargin{1};
+        epsilon = varargin{2};
     end 
     
     [m, n] = hsize(H);
     
-    if isempty(H.D)
-        [L11, U11] = hlu(H.A11, epsilon);
-         U12 = mldivide(L11, H.U1 * H.V2);  %L11 U12 = A12 = H.U1 * H.V2
-         L21 = mrdivide(H.U2 * H.V1, U11);  %L21 U11 = A21 = H.U2 * H.V1
-        U12 = mldivide(L11, H.U1); 
-        L21 = mrdivide(H.V1, U11); 
 
-        X = -H.U2 * (L21 * U12); 
-        NH = hrank_update(H.A22, X, H.V2, epsilon);
-        [L22, U22] = hlu(NH, epsilon);  lu(hadd(H.A22, L21 * U12, '-'));
-        
-        U12 = U12 * H.V2;
-        L21 = H.U2 * L21;
+    if strcmp(oformat, 'dense')
+        if isempty(H.D)
+            [L11, U11] = hlu(H.A11, 'dense', epsilon);
+            U12 = mldivide(L11, H.U1 * H.V2);  %L11 U12 = A12 = H.U1 * H.V2
+            L21 = mrdivide(H.U2 * H.V1, U11);  %L21 U11 = A21 = H.U2 * H.V1
+            U12 = mldivide(L11, H.U1); 
+            L21 = mrdivide(H.V1, U11); 
 
-        L = blkdiag(L11, L22);
-        L(size(L11, 1)+1:end, 1:size(L11, 2)) = L21;
-        L = sparse(L);
-        U = blkdiag(U11, U22);
-        U(1:size(U11, 1), size(U11, 2)+1:end) = U12;
-        U = sparse(U);
+            NH = hrank_update(H.A22, -H.U2 * (L21 * U12), H.V2, epsilon);
+            [L22, U22] = hlu(NH, 'dense', epsilon);  % lu(hadd(H.A22, L21 * U12, '-'));
+            
+            U12 = U12 * H.V2;
+            L21 = H.U2 * L21;
+
+            L = blkdiag(L11, L22);
+            L(size(L11, 1)+1:end, 1:size(L11, 2)) = L21;
+            L = sparse(L);
+            U = blkdiag(U11, U22);
+            U(1:size(U11, 1), size(U11, 2)+1:end) = U12;
+            U = sparse(U);
+        else
+            [L, U] = lu(H.D);
+        end
     else
-        [L, U] = lu(H.D);
+        L = H;
+        U = H;
+        
+        m1 = size(H.U1, 1);
+        m2 = size(H.U2, 1);
+        n1 = size(H.V1, 2);
+
+        L.U1 = zeros(m1,1);
+        L.V2 = zeros(m2,1)';
+        U.U2 = zeros(m2,1);
+        U.V1 = zeros(n1,1)';
+        
+        if isempty(H.D)
+            [L.A11, U.A11] = hlu(H.A11, epsilon);
+            
+            U.U1 = L.A11.inverse_double() * H.U1;  %L11 U12 = A12 = H.U1 * H.V2
+            L.V1 = H.V1*U.A11.inverse_double();  %L21 U11 = A21 = H.U2 * H.V1
+            [L.A22, U.A22] = hlu(hrank_update(H.A22, -L.U2 * (L.V1 * U.U1), H.V2, epsilon), epsilon); 
+    
+        else
+            [L.D, U.D] = lu(H.D);
+        end
     end
 end 
+
+

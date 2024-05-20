e6629d3612e4eb4ef8ca66b0acc45059cfc5a6c5
the null <xinyechenai@gmail.com>
2024-05-20 15:19:45 +0200

    update files 2

--- @mphodlr/hadd.m
+++ @mphodlr/hadd.m
@@ -5,7 +5,7 @@ function C = hadd(varargin)
     Parameters
     --------------------
     A - hodlr | double
-        Input matrix - hodlr class / doudense tyle.
+        Input matrix - hodlr class / dense tyle.
   
     B - hodlr | double
         Input matrix - hodlr class / dense tyle.

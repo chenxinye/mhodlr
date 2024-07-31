e80ce676e43248708d050cf0bbcaa86b407e8bf3
chenxinye <cclcquant@yahoo.com>
2024-07-31 16:35:45 +0200

    add new files

--- err_bound.m
+++
@@ -1,3 +0,0 @@
-function err = err_bound(depth, epsilon)
-    err = (2 * sqrt(2 * depth) + 1) * epsilon;
-end
--- lu_err_bound2.m
+++
@@ -1,3 +0,0 @@
-function err = lu_err_bound2(depth, epsilon, norm_H, norm_L, norm_U)
-    err = 2 * (2^depth - 1) * epsilon * norm_H + 11 * (2^depth - 1) * epsilon * norm_L * norm_U;
-end

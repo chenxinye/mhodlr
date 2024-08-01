10d45e1b18d2ffcdd3e1bb08f5090dad33c15c34
chenxinye <cclcquant@yahoo.com>
2024-08-01 21:23:09 +0200

    update compress

--- @amphodlr/private/mp_compress_m.m
+++ @amphodlr/private/mp_compress_m.m
@@ -1,6 +1,12 @@
 %% FOR TEST ONLY
-function [U, V] = mp_compress_m(A, method, epsilon)
+function [U, V] = mp_compress_m(A, method, vareps, varargin)
     
+    if nargin == 4
+        norm_type = varargin{4};
+    else 
+        norm_type = '2';
+    end 
+
     if min(size(A)) == 0
         U = zeros(size(A,1),0);
         V = zeros(0,size(A,2));
@@ -10,15 +16,26 @@ function [U, V] = mp_compress_m(A, method, epsilon)
     global opt;
     if strcmp(method, 'svd')
         [U, S, V] = svd(full(A), 'econ');
-        S = mchop(S);
-        rnk = sum(abs(diag(S)) > S(1,1) * epsilon);
-        U = mchop(U(:,1:rnk));
-        V = S(1:rnk,1:rnk) * mchop(V(:,1:rnk)');
-        
+        if nargin <= 3 | norm_type == '2'
+            rnk = sum(abs(diag(S)) > S(1,1) * vareps);
+            S = mchop(S);
+            U = mchop(U(:,1:rnk));
+            V = S(1:rnk,1:rnk) * mchop(V(:,1:rnk)');
+        elseif norm_type == 'fro'
+            normf = sum(diag(S).^2);
+            cusm = (normf - cumsum(diag(S).^2)) / normf;
+            rnk = sum(cusm > vareps^2) + 1;
+            S = mchop(S);
+            U = mchop(U(:,1:rnk));
+            V = S(1:rnk,1:rnk) * mchop(V(:,1:rnk)');
+        else
+            error("This norm type is not sopported for truncation. Please use `fro` or `2` for norm type specification.")
+        end 
+
     elseif strcmp(method, 'qr')
         [U, V, P] = qr(A);
         V = mchop(V);
-        rnk = sum(abs(diag(V)) > V(1,1) * epsilon);
+        rnk = sum(abs(diag(V)) > V(1,1) * vareps);
         U = mchop(U(:, 1:rnk));
         V = mchop(V(1:rnk,:)*P');
     end
--- @hodlr/private/compress_m.m
+++ @hodlr/private/compress_m.m
@@ -1,20 +1,38 @@
 %% FOR TEST ONLY
-function [U, V] = compress_m(A, method, epsilon)
+function [U, V] = compress_m(A, method, vareps, varargin)
+
+    if nargin == 4
+        norm_type = varargin{4};
+    else 
+        norm_type = '2';
+    end 
+
     if min(size(A)) == 0
-        U = zeros(size(A,1),0);
-        V = zeros(0,size(A,2));
+        U = zeros(size(A,1), 0);
+        V = zeros(0, size(A,2));
         return;
     end
 
     if strcmp(method, 'svd')
-        [U, S, V] = svd(full(A), 'econ');
-        rnk = sum(abs(diag(S)) > S(1,1) * epsilon);
-        U = U(:,1:rnk);
-        V = S(1:rnk,1:rnk) * V(:,1:rnk)';
+        if nargin <= 3 | norm_type == '2'
+            [U, S, V] = svd(full(A), 'econ');
+            rnk = sum(abs(diag(S)) > S(1,1) * vareps);
+            U = U(:,1:rnk);
+            V = S(1:rnk,1:rnk) * V(:,1:rnk)';
+        elseif norm_type == 'fro'
+            [U, S, V] = svd(full(A), 'econ');
+            normf = sum(diag(S).^2);
+            cusm = (normf - cumsum(diag(S).^2)) / normf;
+            rnk = sum(cusm > vareps^2) + 1;
+            U = U(:,1:rnk);
+            V = S(1:rnk,1:rnk) * V(:,1:rnk)';
+        else
+            error("This norm type is not sopported for truncation. Please use `fro` or `2` for norm type specification.")
+        end 
         
     elseif strcmp(method, 'qr')
         [U, V, P] = qr(A);
-        rnk = sum(abs(diag(V)) > V(1,1) * epsilon);
+        rnk = sum(abs(diag(V)) > V(1,1) * vareps);
         U = U(:, 1:rnk);
         V = V(1:rnk,:)*P';
     end

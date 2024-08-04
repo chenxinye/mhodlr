effbb2d25a5d3727b452c9453d5b00dbcea425f4
the-null <xinyechenai@gmail.com>
2024-08-04 12:12:59 +0200

    Update ParameterizedTestExample.m

--- tests/ParameterizedTestExample.m
+++ tests/ParameterizedTestExample.m
@@ -11,20 +11,7 @@ classdef ParameterizedTestExample < matlab.unittest.TestCase
     
     methods (Test)
         function testDayofyear(testCase,monthNum,dayNum,yearNum)
-            % Convert numeric values to mm/dd/yyyy string
-            % Note: MATLAB will automatically convert numbers to strings
-            % when performing number+string arithmetic
-            dateStr = monthNum + "/" + dayNum + "/" + yearNum;
             
-            % Compute expected result
-            dt = datetime(dateStr,"Format","MM/dd/uuuu");
-            doyExpected = day(dt,"dayofyear");
-            
-            % Compute actual result
-            doyActual = dayofyear(dateStr);
-            
-            % Verify that the actual result matches the expected result
-            testCase.verifyEqual(doyActual,doyExpected)
         end
     end
     

78f68812ce2f09bf7602f0a16c774ab0119de034
the-null <xinyechenai@gmail.com>
2024-08-04 16:38:49 +0200

    Update TestUnits.m

--- tests/TestUnits.m
+++ tests/TestUnits.m
@@ -1,22 +1,4 @@
 classdef TestUnits < matlab.unittest.TestCase
-    % TestExamples contains a set of 4 simple tests:
-    %     1) an equality test for a non-leap year date
-    %     2) an equality test for a leap year date
-    %     3) a negative test for an invalid date format input
-    %     4) a negative test for a correct date format but an invalid date
-    %     5) an equality test for a non-leap year date using the alternate
-    %        dateFormat (COMMENTED OUT)
-    %
-    % Notes:
-    %     A) A negative test verifies that the code errors/fails in an
-    %        expected way (e.g., the code gives the right error for a
-    %        specific bad input)
-    %     B) The 5th test is included for completeness, but is commented 
-    %        out to illustrate missing code coverage in continous
-    %        integration (CI) systems
-
-    % Copyright 2022 The MathWorks, Inc.
-
     methods (Test)
 
         function testBuild(testCase)

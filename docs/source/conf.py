3a6e465f620874821f9e3deb648c8c243d325f6c
the-null <xinyechenai@gmail.com>
2024-08-04 12:38:06 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,6 +1,6 @@
-
 import sys
 import os
+import sphinx_rtd_theme
 
 
 sys.path.insert(0, os.path.abspath('../..'))
@@ -20,7 +20,8 @@ extensions = [
     'sphinx.ext.autosummary',
     'sphinx.ext.autodoc',
     'sphinx.ext.mathjax',
-    'sphinx.ext.viewcode'
+    'sphinx.ext.viewcode',
+    'sphinx_rtd_theme',
 ]
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -35,7 +36,7 @@ pygments_style = 'default'
 
 html_static_path = ['_static']
 html_style = 'css/_.css'
-html_theme = "alabaster" # html_theme = 'alabaster'
+html_theme = "sphinx_rtd_theme" # html_theme = 'alabaster'
 html_logo = "LOGO2.png"
 html_theme_options = {
     'logo_only': False,

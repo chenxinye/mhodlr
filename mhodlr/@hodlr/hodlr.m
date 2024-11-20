9fa29a624c8d9d78877141ffd9a6f91d302d32c0
the-null <xinyechenai@gmail.com>
2024-11-20 11:06:28 +0100

    Update hodlr.m

--- mhodlr/@hodlr/hodlr.m
+++ mhodlr/@hodlr/hodlr.m
@@ -58,7 +58,7 @@ classdef hodlr
         bottom_level {mustBeInteger} = 0
         vareps {mustBeNonNan, mustBeFinite, mustBeNumeric} = 1.0e-12
         min_block_size {mustBeInteger} = 20
-        issparse = true
+        issparse = false
     end
 
     properties(Access=private)

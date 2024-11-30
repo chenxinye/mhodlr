7f7b5dea9abc6e6e90d5c171cf7888255bb67f4a
the-null <xinyechenai@gmail.com>
2024-11-30 14:51:19 +0100

    Update mphodlr.m

--- mhodlr/@mphodlr/mphodlr.m
+++ mhodlr/@mphodlr/mphodlr.m
@@ -201,7 +201,7 @@ classdef mphodlr
             obj.shape(1) = rowSize; 
             obj.shape(2) = colSize;
             
-            if rowSize <= obj.min_block_size | colSize <= obj.min_block_size | level > obj.max_level
+            if floor(rowSize / 2) < obj.min_block_size | floor(colSize / 2) < obj.min_block_size | level > obj.max_level
                 obj.D = A;
                 obj.bottom_level = max(obj.bottom_level, level-1);
                 return;
@@ -231,7 +231,7 @@ classdef mphodlr
             obj.shape(1) = rowSize; 
             obj.shape(2) = colSize;
            
-            if rowSize <= obj.min_block_size | colSize <= obj.min_block_size | level > obj.max_level
+            if floor(rowSize / 2) < obj.min_block_size | floor(colSize / 2) < obj.min_block_size | level > obj.max_level
                 obj.D = A;
                 obj.bottom_level = max(obj.bottom_level, level-1);
                 return;
@@ -262,7 +262,7 @@ classdef mphodlr
             obj.shape(1) = rowSize; 
             obj.shape(2) = colSize;
             
-            if rowSize <= obj.min_block_size | colSize <= obj.min_block_size | level > obj.max_level
+            if floor(rowSize / 2) < obj.min_block_size | floor(colSize / 2) < obj.min_block_size | level > obj.max_level
                 obj.D = A;
                 obj.bottom_level = max(obj.bottom_level, level-1);
                 return;

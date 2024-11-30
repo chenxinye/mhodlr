73649611aa6cea27284e2a00daaff805f9385263
the-null <xinyechenai@gmail.com>
2024-11-30 14:50:08 +0100

    Update hodlr.m

--- mhodlr/@hodlr/hodlr.m
+++ mhodlr/@hodlr/hodlr.m
@@ -164,7 +164,7 @@ classdef hodlr
             obj.shape(1) = rowSize; 
             obj.shape(2) = colSize;
            
-            if rowSize <= obj.min_block_size | colSize <= obj.min_block_size | level > obj.max_level
+            if floor(rowSize / 2) < obj.min_block_size | floor(colSize / 2) < obj.min_block_size | level > obj.max_level
                 obj.D = A;
                 obj.bottom_level = max(obj.bottom_level, level-1);
                 return;

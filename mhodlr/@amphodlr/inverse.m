3a2e547ee0544ded057b15f7c860555d199e6749
the-null <xinyechenai@gmail.com>
2024-12-04 05:56:48 +0100

    Update inverse.m

--- mhodlr/@amphodlr/inverse.m
+++ mhodlr/@amphodlr/inverse.m
@@ -1,4 +1,4 @@
-function C = inverse(obj, varargin)
+function C = inverse(H, varargin)
 %{
     The function is to return the inverse of HOLDR matrix.
     
@@ -18,7 +18,8 @@ function C = inverse(obj, varargin)
     C - hodlr | double
         Return matrix in hodlr class or dense array.
     
-%}
+%}  
+    
     switch nargin
         case 1 
             algorithm = 1;
@@ -35,34 +36,34 @@ function C = inverse(obj, varargin)
     end
 
     if algorithm == 1
-        if strcmp(oformat, 'hodlr')
-            C = inverse_hodlr(obj);
+        if strcmp(oformat, 'hodlr') | strcmp(oformat, 'mphodlr') | strcmp(oformat, 'amphodlr')
+            C = inverse_hodlr(H);
         else
-            C = inverse_dense(obj);
+            C = inverse_dense(H);
         end
     else
-        if strcmp(oformat, 'hodlr')
-            C = inverse_nonrecursive_hodlr(obj);
+        if strcmp(oformat, 'hodlr') | strcmp(oformat, 'mphodlr') | strcmp(oformat, 'amphodlr')
+            C = inverse_nonrecursive_hodlr(H);
         else
-            C = inverse_nonrecursive_dense(obj);
+            C = inverse_nonrecursive_dense(H);
         end
     end
 end 
 
-function C = inverse_nonrecursive_hodlr(obj)
+function C = inverse_nonrecursive_hodlr(H)
     %% This member method implement inverse of hodlr matrix in nonrecursive manner
-    [m, n] = hsize(obj);
+    [m, n] = hsize(H);
 
     if m ~= n
         error('Inverse is only applied to a square HODLR matrix.');
     end
 
-    if isempty(obj.D)
-        C1 = inverse_dense(obj.A11);
-        C2 = inverse_dense(obj.A22);
+    if isempty(H.D)
+        C1 = inverse_dense(H.A11);
+        C2 = inverse_dense(H.A22);
         
-        A12 = obj.U1 * obj.V2;
-        A21 = obj.U2 * obj.V1;
+        A12 = H.U1 * H.V2;
+        A21 = H.U2 * H.V1;
         X = blkoffdiag(C1 * A12, C2* A21);
         [U, D, V] = svd(X);
 
@@ -70,27 +71,27 @@ function C = inverse_nonrecursive_hodlr(obj)
         R = blkdiag(C1, C2);
         C = L * R;
     else
-        C = inv(obj.D);
+        C = inv(H.D);
     end
 
-    [md, td, mbs, ml, tp] = load_params(obj, 0);
+    [md, td, mbs, ml, tp] = load_params(H, 0);
     C = hodlr(C, md, td, mbs, ml, tp);
 end
 
-function C = inverse_nonrecursive_dense(obj)
+function C = inverse_nonrecursive_dense(H)
     %% This member method implement inverse of hodlr matrix in nonrecursive manner
-    [m, n] = hsize(obj);
+    [m, n] = hsize(H);
 
     if m ~= n
         error('Inverse is only applied to a square HODLR matrix.');
     end
     
-    if isempty(obj.D)
-        C1 = inverse_nonrecursive_dense(obj.A11);
-        C2 = inverse_nonrecursive_dense(obj.A22);
+    if isempty(H.D)
+        C1 = inverse_nonrecursive_dense(H.A11);
+        C2 = inverse_nonrecursive_dense(H.A22);
         
-        A12 = obj.U1 * obj.V2;
-        A21 = obj.U2 * obj.V1;
+        A12 = H.U1 * H.V2;
+        A21 = H.U2 * H.V1;
         X = blkoffdiag(C1 * A12, C2* A21);
         [U, D, V] = svd(X);
 
@@ -98,54 +99,54 @@ function C = inverse_nonrecursive_dense(obj)
         R = blkdiag(C1, C2);
         C = L * R;
     else
-        C = inv(obj.D);
+        C = inv(H.D);
     end
     
 end
 
-function C = inverse_hodlr(obj)
-    if ~issquare(obj)
+function C = inverse_hodlr(H)
+    if ~issquare(H)
         error('Inverse is only applied to a square HODLR matrix.');
     end
     
-    C = obj;
-    if isempty(obj.D)
-        X22 = inverse_hodlr(obj.A22);
-        A12 = obj.U1 * obj.V2;
-        A21 = obj.U2 * obj.V1;
+    C = H;
+    if isempty(H.D)
+        X22 = inverse_hodlr(H.A22);
+        A12 = H.U1 * H.V2;
+        A21 = H.U2 * H.V1;
         
-        C.A11  = inverse_hodlr(hadd(obj.A11, hdot_dense(hdot(A12, X22), A21), '-'));
+        C.A11  = inverse_hodlr(hadd(H.A11, hdot_dense(hdot(A12, X22), A21), '-'));
     
-        [C.U1, C.V2] = compress_m(hdot_dense(hdot_dense(C.A11, -A12), X22), obj.method, obj.vareps, obj.max_rnk, obj.trun_norm_tp, obj.issparse);
+        [C.U1, C.V2] = compress_m(hdot_dense(hdot_dense(C.A11, -A12), X22), H.method, H.vareps, H.max_rnk, H.trun_norm_tp, H.issparse);
         C21 = -hdot_dense(hdot_dense(X22, A21), C.A11);
-        [C.U2, C.V1] = compress_m(C21, obj.method, obj.vareps, obj.max_rnk, obj.trun_norm_tp, obj.issparse);
+        [C.U2, C.V1] = compress_m(C21, H.method, H.vareps, H.max_rnk, H.trun_norm_tp, H.issparse);
         XX = hdot_dense(C21 * A12, X22);
         C.A22 = hadd(X22, XX, '-');
     else
-        C.D = inv(obj.D);
+        C.D = inv(H.D);
     end
 end
 
-function C = inverse_dense(obj)
-    if ~issquare(obj)
+function C = inverse_dense(H)
+    if ~issquare(H)
         error('Inverse is only applied to a square HODLR matrix.');
     end
     
-    if strcmp(class(obj), 'amphodlr') | strcmp(class(obj), 'hodlr')
-        if isempty(obj.D)
-            X22 = inverse_dense(obj.A22);
-            A12 = obj.U1*obj.V2;
-            A21 = obj.U2*obj.V1;
-            X11 = inverse_dense(hadd_partial_hodlr(obj.A11, A12 * X22 * A21 ,'-', true));
+    if strcmp(class(H), 'amphodlr') | strcmp(class(H), 'hodlr')
+        if isempty(H.D)
+            X22 = inverse_dense(H.A22);
+            A12 = H.U1*H.V2;
+            A21 = H.U2*H.V1;
+            X11 = inverse_dense(hadd_partial_hodlr(H.A11, A12 * X22 * A21 ,'-', true));
             C12 = -X11 * A12 * X22;
             C21 = -X22 * A21 * X11;
             C22 = X22 + X22 * A21 * X11 * A12 * X22;
 
             C = [X11, C12; C21, C22];
         else
-            C = inv(obj.D);
+            C = inv(H.D);
         end
     else
-        C = inv(obj);
+        C = inv(H);
     end
 end

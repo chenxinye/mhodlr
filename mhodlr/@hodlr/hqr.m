4ce11feecfdc7587b343394153b15ee413cd3cb6
the-null <xinyechenai@gmail.com>
2025-01-21 12:26:49 +0100

    Update hqr.m

--- mhodlr/@hodlr/hqr.m
+++ mhodlr/@hodlr/hqr.m
@@ -1,4 +1,21 @@
 function [Q, R] = hqr(H, method)
+%{
+    The function is used for the operation of summation or subtraction for HODLR matrix A and B.
+
+    Parameters
+    --------------------
+    A - hodlr
+        Input matrix - hodlr class / dense tyle.
+
+    method - str
+        Options: 'lintner', 'bebendorf' and 'kressner'.
+    
+    Returns
+    --------------------
+    Q, R - hodlr 
+        Return matrix in hodlr class.
+%}
+
     if strcmp(method, 'lintner')
         [Q, R] = lintner_qr(H);
 

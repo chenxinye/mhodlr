210225c9a82a4dc44661d90e676cdaad22d8f528
the-null <xinyechenai@gmail.com>
2025-01-24 10:16:49 +0100

    Update add.m

--- mhodlr/add.m
+++ mhodlr/add.m
@@ -1,8 +1,13 @@
 function C = add(A, B, varargin)
     C = hadd(A, B, '+', 'hodlr');
+
+    max_rnk = min(A.max_rnk, B.max_rnk);
+    
     if nargin > 2
         for i = 1:(nargin-2)
             C = hadd(C, varargin{i-2}, '+');
         end
     end
+    
+    C = htruncate(C, max_rnk);
 end

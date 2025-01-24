d57206e233cc4c6cbc9de2baffe5b07d7ec9e7e2
the-null <xinyechenai@gmail.com>
2025-01-24 22:12:33 +0100

    Update mdot.m

--- mhodlr/mdot.m
+++ mhodlr/mdot.m
@@ -1,5 +1,5 @@
 function C = mdot(A, B, varargin)
-  C = hdot(A, B, 'hodlr');
+  C = mhdot(A, B, 'hodlr');
   if nargin > 2
       for i = 1:(nargin-2)
           C = mhdot(C, varargin{i}, 'hodlr');

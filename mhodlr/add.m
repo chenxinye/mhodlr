47e77d1956e195b442d629c35042844782705f82
the-null <xinyechenai@gmail.com>
2025-01-24 10:15:14 +0100

    Update add.m-> continuous add

--- mhodlr/add.m
+++ mhodlr/add.m
@@ -1,3 +1,8 @@
-function C = add(A, B)
+function C = add(A, B, varargin)
     C = hadd(A, B, '+', 'hodlr');
+    if nargin > 2
+        for i = 1:(nargin-2)
+            C = hadd(C, varargin{i-2}, '+');
+        end
+    end
 end

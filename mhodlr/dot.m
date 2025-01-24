5b7cbd63ee200f1a2484b284d0a48b9d519be93e
the-null <xinyechenai@gmail.com>
2025-01-24 22:11:45 +0100

    Update dot.m

--- mhodlr/dot.m
+++ mhodlr/dot.m
@@ -1,3 +1,8 @@
-function C = dot(A, B)
+function C = dot(A, B, varargin)
   C = hdot(A, B, 'hodlr');
+  if nargin > 2
+      for i = 1:(nargin-2)
+          C = hdot(C, varargin{i}, 'hodlr');
+      end
+  end
 end

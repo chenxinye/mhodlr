1f1cd65cc7024001fc30969d10328f0054e8af97
the-null <xinyechenai@gmail.com>
2025-01-26 12:18:09 +0100

    Update mlintner2_qr.m

--- mhodlr/@amphodlr/mlintner2_qr.m
+++ mhodlr/@amphodlr/mlintner2_qr.m
@@ -1,1 +1,8 @@
-
+function [Q, R] = mlintner2_qr(hA)
+    B = mhdot(hA.transpose(), hA);
+    R = mhchol(B);
+    Q = mhtrsu(hA, R, 1);
+    [Q, Rd] = mlintner_qr(Q);
+    Q = hmchop(Q)
+    R = mhdot(Rd, R);
+end

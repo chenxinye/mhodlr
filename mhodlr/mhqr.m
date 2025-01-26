e7dcc09e93e5845833e8163f32189f7519fb05a2
the-null <xinyechenai@gmail.com>
2025-01-26 21:40:01 +0100

    Create mhqr.m

---
+++ mhodlr/mhqr.m
@@ -0,0 +1,31 @@
+function [Q, R] = mhqr(H, method)
+%{
+    The function is to perform QR factorization based on HODLR representation.
+
+    Parameters
+    --------------------
+    A - hodlr
+        Input matrix - hodlr class / dense tyle.
+
+    method - str
+        Options: 'lt', 'lt2' and 'dk'.
+    
+    Returns
+    --------------------
+    Q, R - hodlr 
+        Return matrix in hodlr class.
+%}
+
+    global opt;
+    set_prec(opt);
+    
+    if strcmp(method, 'lt')
+        [Q, R] = mlintner_qr(H);
+    
+    elseif strcmp(method, 'lt2')
+        [Q, R] = mlintner2_qr(H);
+
+    elseif strcmp(method, 'kd')
+        [Q, R] = mkressner_qr(H);
+    end
+end

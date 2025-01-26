5defac9b1e7ecc803dad9cf020b44cb0c55c784d
the-null <xinyechenai@gmail.com>
2025-01-26 12:28:00 +0100

    Update mhqr.m

--- mhodlr/@mphodlr/mhqr.m
+++ mhodlr/@mphodlr/mhqr.m
@@ -19,13 +19,13 @@ function [Q, R] = mhqr(H, method)
 
     global opt;
     set_prec(opt);
-    if strcmp(method, 'lintner')
+    if strcmp(method, 'lt')
         [Q, R] = mlintner_qr(H);
+    
+    elseif strcmp(method, 'lt2')
+        [Q, R] = mlintner2_qr(H);
 
-    elseif strcmp(method, 'bebendorf')
-        [Q, R] = bebendorf_qr(H);
-
-    elseif strcmp(method, 'kressner')
+    elseif strcmp(method, 'kd')
         [Q, R] = mkressner_qr(H);
     end
 end

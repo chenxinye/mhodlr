cbec3c9ea9a1c69f3235735c1a03077c4e0d4829
the-null <xinyechenai@gmail.com>
2025-01-30 15:09:37 +0100

    Update bf_qr.m

--- mhodlr/@hodlr/bf_qr.m
+++ mhodlr/@hodlr/bf_qr.m
@@ -6,14 +6,12 @@ function [Q, R] = bf_qr(hA)
            error('Only square matrix is supported.')
        end
 
-       M12 = hA.U1 * hA.V2;
-       M21 = hA.U2 * hA.V1;
-       X = hdot(M21, hA.A11.inverse())
+       H12 = hA.U1 * hA.V2;
+       H21 = hA.U2 * hA.V1;
+       X = hdot(H21, hA.A11.inverse)
        XTX =  hdot(X.transpose(), X)
        XXT =  hdot(X, X.transpose())
-       disp(size(XTX.dense))
-
-       disp(size(XXT.dense))
+       
        IXX1 = hadd(XTX, eye(n1), '+');
        IXX2 = hadd(XXT, eye(m2), '+');
 
@@ -22,28 +20,41 @@ function [Q, R] = bf_qr(hA)
 
        R11 = hdot(L1, hA.A11);
 
-       R12 = M12 + X.transpose().dense * hA.A22.dense;
-       R12 = hdot(L1.transpose().inverse(), R12, 'dense');
+       B1 = hadd(M12, hdot(X.transpose, hA.A22));
+       R12 = lu_solve(L1, B1);
 
        R22 = hadd(hA.A22, hdot(X, M12), '-');
-       R22 = hdot(L2.transpose().inverse.dense, R22);
+       R22 = lu_solve(L2.transpose, R22);
        
        [Q1, R11] = bf_qr(R11);
        R12 = hdot(Q1.transpose(), R12, 'dense');
 
        [Q2, R22] = bf_qr(R22);
        
-       Q11 = hdot(L1.inverse(), Q1, 'dense');
-       Q12 = hdot(L2.inverse(), Q2, 'dense');
-       Q21 = hdot(L1.inverse(), Q1, 'dense');
-       Q22 = hdot(L2.inverse(), Q2, 'dense');
-
+       Q11 = hdot(L1.inverse, Q1);
+       Q22 = hdot(L2.inverse, Q2);
+       
+       Q21 = hdot(L1.inverse, Q1, 'dense');
+       Q12 = hdot(L2.inverse, Q2, 'dense');
+       
        Q12 = hdot(X.transpose(), -Q12, 'dense');
        Q21 = hdot(X, Q21, 'dense');
-        
-       Q = hodlr([Q11, Q12; Q21, Q22], hA.bottom_level);
-       R = hodlr([recover(R11), R12; zeros(m2, m1), recover(R22)], hA.bottom_level);
 
+       Q.A11 = Q11;
+       Q.A22 = Q22;
+       
+       [Q.U1, Q.V2] = hA.compress(Q12);
+       [Q.U2, Q.V1] = hA.compress(Q21);
+       
+       Q = hA.bottom_level;
+
+       R.A11 = R11;
+       R.A22 = R22;
+
+       [R.U1, R.V2] = hA.compress(R12);
+       
+       R.U2 = zeros(m2, 1);
+       R.V1 = zeros(1, m1);
     else
         [Q, R] = qr(hA.D);
         Q = hodlr(Q, 0);

ea141a51840898fe5fb0244c87ae7d634307d4de
the-null <xinyechenai@gmail.com>
2025-02-21 16:20:38 +0100

    Update mlrsolve.m

--- mhodlr/mlrsolve.m
+++ mhodlr/mlrsolve.m
@@ -1,1 +1,9 @@
-
+function x = lrsolve(A, b, option)
+  if strcmp(option, "qr")
+    x = mqr_solve(A, b);
+  elseif strcmp(option, "lu")
+    x = mlu_solve(A, b);
+  else
+    x = mpm_solve(A, b);
+  end
+end
